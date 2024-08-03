a4ee40a73ada0271cf6a5b0fd0823cbab25303e2
Reevu Thapa <137419757+reevubabbai2003@users.noreply.github.com>
2024-08-03 10:33:23 +0530

    Add files via upload

---
+++ worksheet2_me.R
@@ -0,0 +1,34 @@
+# Question 1
+# find factorial of n
+fact = function(n)
+{
+  ans = 1
+  for(i in 1:n)
+  {
+    ans = ans*i
+  }
+  return(ans)
+}
+# another way
+fact2 = function(n)
+{
+  prod(1:n)
+}
+# Question 2
+func = function(n)
+{
+  
+}
+# Question 3
+seat <- read.csv("https://dvats.github.io/assets/course/mth208/seating.csv")
+roll_no = function(roll)
+{
+  result = find()
+} 
+# Question 5
+str(seat)
+?str
+check = seat$Roll.No. < 1e7
+length(check)
+sum(check)
+
